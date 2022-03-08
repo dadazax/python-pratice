0cdb5a7e9cc3da2502f437e61fbf95f94ed0abfd
dadazax <101158889+dadazax@users.noreply.github.com>
2022-03-08 14:11:43 +0800

    Create func-solution.py

---
+++ func-solution.py
@@ -0,0 +1,15 @@
+#解二元一次方程式
+
+import cmath
+
+print('ax**2+bx+c=0')
+a=float(input('a='))
+if a == 0:
+    print('a不能=0')
+else:
+    b=float(input('b='))
+    c=float(input('c='))
+d=print(cmath.sqrt(b**2-4*a*c))
+x1 = (-b-cmath.sqrt(b**2-4*a*c))/(2*a)
+x2 = (-b+cmath.sqrt(b**2-4*a*c))/(2*a)
+print(f'the answer x is {x1} or {x2}')
